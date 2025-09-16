c13d46b3c419ad2ab299b37a401a7d14d52274b0
bhfnasralla <73263495+bhfnasralla@users.noreply.github.com>
2025-09-16 13:31:11 -0300

    Update wazuh_data_type.go
    Adding new eventtypes on jumpcloud integration with Wazuh

--- pkg/wazuh_data_type.go
+++ pkg/wazuh_data_type.go
@@ -39,3 +39,53 @@ func (d *JumpCloudAdminEvent) convertToWazuhString() string {
 	b, _ := json.Marshal(d)
 	return string(b)
 }
+
+// Novos eventos adicionados
+func (d *JumpCloudMDMEvent) convertToWazuhString() string {
+    d.JumpCloudEventType = "mdm"
+    b, _ := json.Marshal(d)
+    return string(b)
+}
+
+func (d *JumpCloudNotificationChannelEvent) convertToWazuhString() string {
+    d.JumpCloudEventType = "notification_channel"
+    b, _ := json.Marshal(d)
+    return string(b)
+}
+
+func (d *JumpCloudObjectStorageEvent) convertToWazuhString() string {
+    d.JumpCloudEventType = "object_storage"
+    b, _ := json.Marshal(d)
+    return string(b)
+}
+
+func (d *JumpCloudAlertEvent) convertToWazuhString() string {
+    d.JumpCloudEventType = "alert"
+    b, _ := json.Marshal(d)
+    return string(b)
+}
+
+func (d *JumpCloudSaaSManagementEvent) convertToWazuhString() string {
+    d.JumpCloudEventType = "saas_management"
+    b, _ := json.Marshal(d)
+    return string(b)
+}
+
+func (d *JumpCloudAccessManagementEvent) convertToWazuhString() string {
+    d.JumpCloudEventType = "access_management"
+    b, _ := json.Marshal(d)
+    return string(b)
+}
+
+func (d *JumpCloudReportsEvent) convertToWazuhString() string {
+    d.JumpCloudEventType = "reports"
+    b, _ := json.Marshal(d)
+    return string(b)
+}
+
+func (d *JumpCloudAssetManagementEvent) convertToWazuhString() string {
+    d.JumpCloudEventType = "asset_management"
+    b, _ := json.Marshal(d)
+    return string(b)
+}
+
